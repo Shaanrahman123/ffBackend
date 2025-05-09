b7b690b52d6486309f467c053eea4441853c15ef
Shaanrahman123 <coderraufur@gmail.com>
2025-05-09 18:07:39 +0530

    updated passenger lead

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ const bookingRoutes = require("./booking.routes");
 const adminRoutes = require("./admin.routes");
 const topRoute = require("./topFlightRoute.routes");
 const transactionRoute = require("./transaction.routes");
+const passengerDetailsRoutes = require("./passengerDetails.route");
 
 const router = express.Router();
 
@@ -26,7 +27,7 @@ router.use("/booking", bookingRoutes);
 router.use("/testimonial", testimonialRoute);
 router.use("/contact", contactRoute);
 router.use("/toproute", topRoute);
-
+router.use("/passenger-details", passengerDetailsRoutes);
 router.use("/admin", adminRoutes);
 
 module.exports = router;
